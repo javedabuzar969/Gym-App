3516f2354cc6dbeaf853c35eba54e69071cf0837
javedabuzar969 <javedabuzar969@gmail.com>
2026-06-26 07:53:26 -0700

    g

--- 01_add_contact_to_members.sql
+++
@@ -1,2 +0,0 @@
--- Add 'contact' column to the 'members' table if it doesn't exist
-ALTER TABLE public.members ADD COLUMN IF NOT EXISTS contact TEXT;
--- CHECK_TABLES.sql
+++
@@ -1,4 +0,0 @@
--- Run this to verify if tables exist
-SELECT table_name 
-FROM information_schema.tables 
-WHERE table_schema = 'public';
